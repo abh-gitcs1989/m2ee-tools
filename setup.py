563dfbfa105cc3109bac5a9dc40659341b4d04a0
Jouke Waleson <jouke.waleson@mendix.com>
2014-04-24 21:38:25 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name             = "m2ee",
+    version          = "0.5.9",
+    description      = "Run and administer Mendix apps",
+    author           = "mendix",
+    url              = "https://github.com/mendix/m2ee-tools",
+    packages         = ['m2ee'],
+    package_dir      = {'m2ee': 'src/m2ee'},
+    scripts          = ['src/m2ee.py'],
+)
